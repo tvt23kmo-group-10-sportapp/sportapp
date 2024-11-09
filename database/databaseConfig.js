34deb51131abb1af13f7815cec51a299e8a7c8c9
Ville-A <ville.m.ahola@gmail.com>
2024-11-10 00:19:59 +0200

    working app with added settings screen from Mikko branch

--- database/databaseConfig.js
+++ database/databaseConfig.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: FIREBASE_API_KEY,
